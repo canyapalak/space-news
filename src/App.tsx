9824ba5222ea57b268035f9945262559f2e63f0a
santiagomanso <santiagomanso@web.de>
2023-04-06 20:21:04 +0200

    🔧 reworked: overflow-auto when mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './views/Home';
-import NewsPage from './views/NewsPage';
-import BlogsPage from './views/BlogsPage';
-import ReportsPage from './views/ReportsPage';
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './views/Home'
+import NewsPage from './views/NewsPage'
+import BlogsPage from './views/BlogsPage'
+import ReportsPage from './views/ReportsPage'
 
 function App() {
   return (
-    <div className="App font-navine h-screen w-screen bg-gradient-to-br 
-    from-black/80 to-slate-700 text-slate-100">
+    <div
+      className='App font-navine h-screen w-screen bg-gradient-to-br 
+    from-black/80 to-slate-700 text-slate-100 overflow-auto'
+    >
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/blogs" element={<BlogsPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/news' element={<NewsPage />} />
+        <Route path='/blogs' element={<BlogsPage />} />
+        <Route path='/reports' element={<ReportsPage />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
