0adda9b4d33204260f73c4a2d241426309ea0892
canyapalak <canyapalak@gmail.com>
2023-04-08 01:36:23 +0200

    ➕ installed react-icons

--- src/views/NewsDetailsPage.tsx
+++ src/views/NewsDetailsPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import Modal from '../components/Modal'
 import { Link } from "react-router-dom";
+import { TbArrowBackUp } from "react-icons/tb"
 
 export default function NewsDetailsPage() {
     const { id } = useParams();
@@ -62,30 +63,33 @@ export default function NewsDetailsPage() {
                 {!newsDetails ? (
                     <div className='mt-5 mx-auto items-center'><Loading /></div>
                 ) : (
-                    <div className='p-5 w-[20rem] md:w-[32rem] lg:w-[53rem] mt-5 flex flex-col bg-gradient-to-br from-stone-950 to-stone-700 
-                border-solid border-2 border-stone-600 rounded-xlshadow-md items-center rounded-lg gap-3'>
-                        <div className="w-[18rem] md:w-[30rem] lg:w-[50rem]">
-                            <img src={newsDetails.image_url} alt="News Image"
-                                className="rounded-lg object-cover w-full"
-                            />
-                        </div>
-                        <div className='w-full flex flex-col mx-auto md:flex-grow md:text-left ml-0 gap-2'>
-                            <div className="flex flex-row">
-                                <p>{new Date(newsDetails.published_at).toLocaleDateString('en-US', {
-                                    year: 'numeric',
-                                    month: '2-digit',
-                                    day: '2-digit'
-                                })}</p>
-                                <p className="ml-auto mr-2">Source: </p>
-                                <button className="bg-cyan-500 hover:bg-cyan-400 rounded-md px-1 text-black"
-                                    onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-                                        handleClick(newsDetails)
-                                    }>
-                                    {newsDetails.news_site}</button>
+                    <div>
+                        <Link to="/news"><TbArrowBackUp className="cursor-pointer w-6 h-6 inline-block" /></Link>
+                        <div className='p-5 w-[20rem] md:w-[32rem] lg:w-[53rem] mt-5 flex flex-col bg-gradient-to-br from-stone-950 to-stone-700 
+                    border-solid border-2 border-stone-600 rounded-xlshadow-md items-center rounded-lg gap-3'>
+                            <div className="w-[18rem] md:w-[30rem] lg:w-[50rem]">
+                                <img src={newsDetails.image_url} alt="News Image"
+                                    className="rounded-lg object-cover w-full"
+                                />
                             </div>
-                            <div>
-                                <p className='text-lg text-orange-200'>{newsDetails.title}</p>
-                                <p className='mt-3 md:mt-1 '>{newsDetails.summary}</p>
+                            <div className='w-full flex flex-col mx-auto md:flex-grow md:text-left ml-0 gap-2'>
+                                <div className="flex flex-row">
+                                    <p>{new Date(newsDetails.published_at).toLocaleDateString('en-US', {
+                                        year: 'numeric',
+                                        month: '2-digit',
+                                        day: '2-digit'
+                                    })}</p>
+                                    <p className="ml-auto mr-2">Source: </p>
+                                    <button className="bg-cyan-500 hover:bg-cyan-400 rounded-md px-1 text-black"
+                                        onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+                                            handleClick(newsDetails)
+                                        }>
+                                        {newsDetails.news_site}</button>
+                                </div>
+                                <div>
+                                    <p className='text-lg text-orange-200'>{newsDetails.title}</p>
+                                    <p className='mt-3 md:mt-1 '>{newsDetails.summary}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
